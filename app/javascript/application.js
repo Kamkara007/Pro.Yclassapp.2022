6265a96c6514c8c6d9e983d1527fe3322fc21875
kamkara <lnclass.kamkara@gmail.com>
2023-06-08 16:51:09 +0000

    app: Add Show

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,23 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+
+// Feed course show
+
+
+document.addEventListener('turbo:load', function() {
+  const viewMoreLinks = document.querySelectorAll('.feed_course_summary_content');
+
+  viewMoreLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      const summaryDiv = this.parentElement;
+      const fullContentDiv = summaryDiv.nextElementSibling;
+
+      summaryDiv.classList.add('hidden');
+      fullContentDiv.classList.remove('hidden');
+    });
+  });
+});
