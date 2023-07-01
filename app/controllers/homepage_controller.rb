ddfb3202bc694e5d81cfbd4302ec692178365dff
kamkara <lnclass.kamkara@gmail.com>
2023-07-01 20:13:56 +0000

    app/controllers/homepage_controller: Add Desc ordered

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,10 +1,10 @@
 class HomepageController < ApplicationController
   def index
     @feed_materials = Material.all
-    @feed_courses = Course.all
+    @feed_courses = Course.all.order('created_at DESC')
   end
   def feed
     @feed_materials = Material.all
-    @feed_courses = Course.all
+    @feed_courses = Course.all.order('created_at DESC')
   end
 end
